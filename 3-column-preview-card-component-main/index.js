10be740ada3bb797afd196d8f9101b40a2468393
OD44 <138674075+OD44@users.noreply.github.com>
2023-07-31 16:10:15 +0100

    Add files via upload

---
+++ 3-column-preview-card-component-main/index.js
@@ -0,0 +1,14 @@
+// const amount = [20, 40, 42, 14, 16]
+// let a = 0
+// while(a<2){
+//     if(amount[a]>=10){
+//         console.log(amount[a]);
+//     }
+//     a++
+// }
+const amount = [20, 40, 155, 70,30]
+let  a = 0
+while(a<amount.length){
+    console.log(amount[a]);
+    a++;
+}
